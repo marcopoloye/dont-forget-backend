6cc9aac5c0ef856b8c2712f3e2314bdbb0c93b2f
Marco Ye <marco.ye99@gmail.com>
2022-09-07 03:59:47 -0400

    rewrote code for heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-module.exports = {
+const connections = {
+  development: {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
@@ -7,5 +8,14 @@ module.exports = {
       database: 'userbase',
       charset: 'utf8'
     },
-
+  },
+  production: {
+    client: 'mysql',
+    connection: process.env.JAWSDB_URL
+  },
 };
+
+module.exports =
+  process.env.NODE_ENV === 'production'
+    ? connections.production
+    : connections.development;
