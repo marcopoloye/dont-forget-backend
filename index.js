9b961ddc7d38a4193da7eb5e70e3c7f073d60067
Marco Ye <marco.ye99@gmail.com>
2022-09-16 19:34:24 -0400

    changed port

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const fallItems = require('./routes/fallItems');
 const springItems = require('./routes/springItems');
 const userbase = require('./routes/userbase');
 
+const PORT = process.env.PORT || 8080;
+
 app.use(express.json());
 app.use(cors());
 
 app.use('/', summerItems, winterItems, fallItems, springItems, userbase);
 
 
-app.listen(8080, () => {
-    console.log(`server is running! @ 8080`);
+app.listen(PORT, () => {
+    console.log(`server is running! @ ${PORT}`);
 });
