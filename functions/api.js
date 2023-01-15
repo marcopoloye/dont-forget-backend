dcc3788bad6ff09be4f81145af3b3cfba74a5964
Marco Ye <marco.ye99@gmail.com>
2023-01-15 01:50:58 -0500

    fixed cors access control error(?)

--- functions/api.js
+++ functions/api.js
@@ -6,30 +6,35 @@ const app = express();
 const router = express.Router();
 
 router.get('/test', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     res.json({
         'test': 'test'
     });
 })
 
 router.get('/fallitems', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     const fallItems = readFallItems();
 
     res.status(200).json(fallItems);
 });
 
 router.get('/springitems', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     const springItems = readSpringItems();
 
     res.status(200).json(springItems);
 });
 
 router.get('/summeritems', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     const summerItems = readSummerItems();
 
     res.status(200).json(summerItems);
 });
 
 router.get('/winteritems', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
     const winterItems = readWinterItems();
 
     res.status(200).json(winterItems);
