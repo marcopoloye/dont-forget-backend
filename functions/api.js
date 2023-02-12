3ebec17576202929b336f45e38d5f61ea76e846d
Marco Ye <marco.ye99@gmail.com>
2023-02-12 01:43:42 -0500

    disabled registration

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,9 @@ const serverless = require('serverless-http');
 const fs = require('fs');
 const app = express();
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const knex = require('knex')(require('../database/knexfile'));
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+// const knex = require('knex')(require('../database/knexfile'));
 
 app.use('/', router);
 
